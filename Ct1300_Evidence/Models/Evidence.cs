6d62dff4260ccf0c002fcd00deebbb7184ab1624
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-03-21 13:18:02 +0100

    rozp

--- Ct1300_Evidence/Models/Evidence.cs
+++ Ct1300_Evidence/Models/Evidence.cs
@@ -4,13 +4,22 @@
     {
         public List<Polozky> Polozky { get; set; } = new List<Polozky>();
         public Polozky Polozka { get; set; } = new Polozky();
-
+        public string Vypis { get; set; }
         public void Pridat()
         {
             //Polozky.Add(Polozka);
             //Polozka = new Polozky();
 
             Polozky.Add(new Polozky(datum: Polozka.Datum, Polozka.Naklady, Polozka.Vynosy));
+        }
+
+        public void ZobrazPocetZaznamu()
+        {
+            Vypis = $"Počet záznamů je {Polozky.Count}";
+        }
+        public void ZobrazitZaznamy()
+        {
+            Vypis = $"Jednotlivé záznamy: <br> {string.Join("<br>", Polozky)}";
         }
     }
 }
